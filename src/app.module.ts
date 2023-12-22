4da8feaa92349f0668e6cdae6e1b6ff202b703eb
Andrii Kartava <andrii.kartava@gmail.com>
2023-12-22 22:17:24 +0200

    Removes unused controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { ScheduleModule } from "@nestjs/schedule";
 import { DataCollectorModule } from "./data-collector/data-collector.module";
 
 @Module({
   imports: [ScheduleModule.forRoot(), DataCollectorModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
